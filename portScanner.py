4cb66231c9d1aabc26c6fe39cacddddcf43f3568
noxi <noelw19@outlook.com>
2022-08-18 03:56:21 +1200

    added threading for performance and now checking all ports.

--- portScanner.py
+++ portScanner.py
@@ -2,14 +2,17 @@
 
 import socket
 import os
+import threading
+from queue import Queue
 
-# AF_INET specifies we are working with ipv4 addresses
-# SOCK_STREAM specifies type of connection (TCP(handshake))
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.settimeout(5)
+# s.settimeout(10)
+
+print_lock = threading.Lock()
 #setup host to scan
 host = ""
 port = 139
+global total
+total = 0
 
 def gethost():
     hostInput = input("Enter the ipv4 address to scan.\nleave blank for loopback address.\n")
@@ -20,31 +23,61 @@ def gethost():
         print(f"{hostInput} is the ip being scanned \n")
         return hostInput
 
-def getPort():
-    print()
-    portInput = input("Enter the port to check.\nleave blank for port 80.\n")
-    if(portInput == "default" or portInput == "Default" or portInput == ""):
-        print("80 is the port being scanned \n")
-        return 80
-    else:
-        print(f"{portInput} is the port being scanned \n")
-        return int(portInput)
 
 def portScanner(port):
-    if s.connect_ex((host, port)):
-        print('port is closed')
-    else:
-        print('port is open')
+    # AF_INET specifies we are working with ipv4 addresses
+    # SOCK_STREAM specifies type of connection (TCP(handshake))
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # print(f"trying {port}")        
+    global total
 
-def clearConsole():
-    command = 'clear'
-    if os.name in ('nt', 'dos'):  # If Machine is running on Windows, use cls
-        command = 'cls'
-    os.system(command)
+    try:
+        conn = s.connect((host, port))
+        with print_lock:
+            print(f'{port}\tOPEN')
+                
+            total += 1
+        conn.close()
+    except:
+        pass
+
+def threader():
+    while True:
+        worker = q.get()
+        portScanner(worker)
+            
+        q.task_done()
+
+q = Queue()
 
 host = gethost()
-port = getPort()
 
-clearConsole()
-print(f"scanning ip: {host} \nport: {port}\n")
-portScanner(port)
+print("Port\tState\n")
+
+for x in range(30):
+    t = threading.Thread(target=threader)
+    t.daemon = True        
+    t.start()
+
+for worker in range(1, 65537):
+    q.put(worker)
+
+q.join()
+
+if total == 0:
+    print("no ports available")
+else:
+    print(f"\n{total} ports open.")
+
+while True:
+    portNum = input("\ncheck a service via port number.\nType exit to quit.\n")
+    if(portNum == 'exit'):
+        print("\n\nThanks For using my port scanner.\nCreator: Noel\n\nGoodbye.")
+        break
+    try:
+        serv = socket.getservbyport(int(portNum), 'tcp')
+        print(f"\nport {portNum} is running {serv}")
+    except:
+        print(f"\nport {portNum} service is receiving an error.\nCannot retrieve service the port is listening for.")
+    
+
